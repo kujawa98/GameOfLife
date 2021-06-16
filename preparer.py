38ba23ec561b8a6ce7de179727827ad8935ddfe7
Kacper Kujawa <kacper.kujawa98@gmail.com>
2021-06-16 13:30:16 +0200

    Refactored preparer

--- preparer.py
+++ preparer.py
@@ -5,24 +5,26 @@ def next_state(cells):
     for row in cells:
         for cell in row:
             if cell.neighbours > 0:
-                nei = neighboors(cells, cell)
-                if (nei > 3 or nei < 2) and cell.is_alive:
-                    cell.kill()
-                elif nei == 3 and not cell.is_alive:
-                    cell.revive()
-                else:
-                    pass
+                count_state(cells, cell)
             elif cell.neighbours == 0 and cell.is_alive:
                 cell.kill()
-
     for row in cells:
         for cell in row:
             prev = cell.is_alive
             cell.iterate()
-            if cell.is_alive and cell.is_alive != prev:
-                add_neighbour(cells, cell, 1)
-            elif not cell.is_alive and cell.is_alive != prev:
-                add_neighbour(cells, cell, -1)
+            if cell.is_alive != prev:
+                if cell.is_alive:
+                    add_neighbour(cells, cell, 1)
+                else:
+                    add_neighbour(cells, cell, -1)
+
+
+def count_state(cells, cell):
+    nei = neighboors(cells, cell)
+    if (nei > 3 or nei < 2) and cell.is_alive:
+        cell.kill()
+    elif nei == 3 and not cell.is_alive:
+        cell.revive()
 
 
 def neighboors(cells, cell):
