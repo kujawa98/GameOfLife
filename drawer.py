cbf0480b27c2ebb75a5dd025d555cd516d80013b
Kacper Kujawa <kacper.kujawa98@gmail.com>
2021-07-16 11:10:54 +0200

    Change shape to circle and delete menu drawing

--- drawer.py
+++ drawer.py
@@ -4,29 +4,12 @@ from setup import *
 def draw(cells, iteration):
     window.fill(BLUE)
     draw_board(cells)
-    draw_menu()
-    write(iteration)
 
 
 def draw_board(cells):
     for i in cells:
         for cell in i:
             if cell.is_alive:
-                pygame.draw.rect(window, WHITE, (cell.x * CELL_WIDTH, cell.y * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT))
-
-
-def draw_menu():
-    pygame.draw.rect(window, LIMON, (0, 570, WINDOW_WIDTH, 150))
-
-
-def write(iteration):
-    it = FONT.render("Iteration     " + str(iteration), True, BLACK)
-    space = FONT.render("Space     to     pause", True, BLACK)
-    c = FONT.render("C    to     clear    screen", True, BLACK)
-    n = FONT.render("N     to     next    step", True, BLACK)
-    r = FONT.render("R     to     randomize", True, BLACK)
-    window.blit(it, (100, 585))
-    window.blit(space, (500, 585))
-    window.blit(c, (500, 615))
-    window.blit(n, (500, 645))
-    window.blit(r, (500, 675))
+                pygame.draw.circle(window, WHITE,
+                                   (cell.x * 2 * CELL_RADIUS + CELL_RADIUS, cell.y * 2 * CELL_RADIUS + CELL_RADIUS),
+                                   CELL_RADIUS)
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import pygame
 pygame.init()
 
 # window properties
-WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 570 + 150
+WINDOW_WIDTH, WINDOW_HEIGHT = 600, 800
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('GoL')
 
@@ -15,9 +15,8 @@ LIMON = (147, 246, 0)
 BLACK = (0, 0, 0)
 
 # board properties
-BOARD_WIDTH, BOARD_HEIGHT = 128, 57
+BOARD_WIDTH, BOARD_HEIGHT = 60, 80
 
 # others
 FPS = 60
-CELL_WIDTH, CELL_HEIGHT = 10, 10
-FONT = pygame.font.Font('./ARCADECLASSIC.TTF', 32)
+CELL_RADIUS = 5
