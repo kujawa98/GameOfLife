c0126a648e3fe5fc45d1e5c611a9c835bc2c231c
Kacper Kujawa <kacper.kujawa98@gmail.com>
2021-07-16 20:51:49 +0200

    Change color setting for cell

--- drawer.py
+++ drawer.py
@@ -1,5 +1,4 @@
 from setup import *
-from random import choice
 
 
 def draw(cells):
@@ -11,10 +10,26 @@ def draw_board(cells):
     for i in cells:
         for cell in i:
             if cell.is_alive:
-                pygame.draw.circle(window, choice(COLORS),
+                pygame.draw.circle(window, color(cell),
                                    (cell.x * CELL_DIAMETER + CELL_RADIUS, cell.y * CELL_DIAMETER + CELL_RADIUS),
                                    CELL_RADIUS)
             else:
                 pygame.draw.circle(window, WHITE,
                                    (cell.x * CELL_DIAMETER + CELL_RADIUS, cell.y * CELL_DIAMETER + CELL_RADIUS),
                                    1, 1)
+
+
+def color(cell):
+    plc = cell.x * CELL_DIAMETER + CELL_RADIUS
+    if plc < 100:
+        return RED
+    elif plc < 2 * 100:
+        return ORANGE
+    elif plc < 3 * 100:
+        return YELLOW
+    elif plc < 4 * 100:
+        return GREEN
+    elif plc < 5 * 100:
+        return BLUE
+    else:
+        return PURPLE
